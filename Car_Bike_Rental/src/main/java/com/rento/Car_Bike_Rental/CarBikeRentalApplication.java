a0d6673eed0740af2763057f988d49f79163cd52
ankitqontenttai <ankit.sharma@qontenttai.com>
2024-11-10 19:32:38 +0530

    intial commit

---
+++ Car_Bike_Rental/src/main/java/com/rento/Car_Bike_Rental/CarBikeRentalApplication.java
@@ -0,0 +1,13 @@
+package com.rento.Car_Bike_Rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBikeRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBikeRentalApplication.class, args);
+	}
+
+}
---
+++ Car_Bike_Rental/src/test/java/com/rento/Car_Bike_Rental/CarBikeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rento.Car_Bike_Rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBikeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
